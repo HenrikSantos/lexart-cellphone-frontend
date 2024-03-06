39a7bddd7d04f0b3046b4af6d55dc7c0601eda9d
Henrik Santos <henrik.ruan4@gmail.com>
2024-03-06 19:30:48 -0300

    ✨ filters: query, name and price asc desc.

--- src/app/cellphones/page.tsx
+++ src/app/cellphones/page.tsx
@@ -1,21 +1,24 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useStore } from "zustand";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import useCellphoneStore, { Cellphone } from "@/global/store";
-import getCellphones from "@/utils/getCellphones";
+import getCellphones from "@/api/getCellphones";
 import getToken from "@/utils/getToken";
+import DeleteProductButtom from "@/components/DeleteCellphoneButtom/DeleteProductButtom";
 
 export default function Cellphones() {
     const router = useRouter();
-    const { setStoreCellphones } = useStore(useCellphoneStore);
+    const { storeCellphones, setStoreCellphones } = useStore(useCellphoneStore);
     const [cellphones, setCellphones] = useState<Cellphone[]>([]);
+    const [sortBy, setSortBy] = useState("price-asc");
+    const [query, setQuery] = useState("");
 
     useEffect(() => {
         const handleGetCellphones = async() => {
-            const token = getToken(); 
+            const token = getToken();
             if (!token) {
                 router.push("/");
 
@@ -29,28 +32,67 @@ export default function Cellphones() {
         handleGetCellphones();
     }, [router, setStoreCellphones]);
 
-    if (!cellphones.length) {
-        return <p>Carregando...</p>;
-    }
+    const sortCellphones = (type: string) => {
+        const sortedCellphones = [...cellphones];
+        if (type === "name-asc") {
+            sortedCellphones.sort((a, b) => a.name.localeCompare(b.name));
+        } else if (type === "name-desc") {
+            sortedCellphones.sort((a, b) => b.name.localeCompare(a.name));
+        }else if (type === "price-asc") {
+            sortedCellphones.sort((a, b) => a.options[0].price - b.options[0].price);
+        } else if (type === "price-desc") {
+            sortedCellphones.sort((a, b) => b.options[0].price - a.options[0].price);
+        }
+        setSortBy(type);
+        setCellphones(sortedCellphones);
+    };
+
+    const handleFilter = useCallback(() => {
+        const filteredCellphones = storeCellphones.filter(cellphone =>
+            cellphone.name.toLowerCase().includes(query.toLowerCase())
+        );
+        setCellphones(filteredCellphones);
+    }, [query, storeCellphones]);
+
+    useEffect(() => {
+        handleFilter();
+    }, [query, handleFilter]);
 
     return (
-        <main className="flex">
-            {cellphones.map((cellphone) => 
-                <div className="m-2 border-2 p-2" key={cellphone.id}>
-                    <h2>{cellphone.name}</h2>
-                    <p>{cellphone.brand} - {cellphone.model}</p>
-                    <p>Cores:</p>
-                    {cellphone.options.map((option) => 
-                        <div key={option.id}>
-                            <p>Preço: {option.price}</p>
-                            <p>Cor: {option.color}</p>
+        <main>
+            <div>
+                <p>Filtrar:</p>
+                <input type="text" name="query" id="query" onChange={(e) => setQuery(e.target.value)}/>
+                <button onClick={() => sortCellphones("name-asc")}>Nome (A-Z)</button>
+                <button onClick={() => sortCellphones("name-desc")}>Nome (Z-A)</button>
+                <button onClick={() => sortCellphones("price-asc")}>Preço Crescente</button>
+                <button onClick={() => sortCellphones("price-desc")}>Preço Decrescente</button>
+                <p className="float-right">Atual: {sortBy}</p>
+            </div>
+            <br />
+            <div className="flex">
+                {cellphones.map((cellphone) => 
+                    <div className="m-2 rounded-md border-2 p-2" key={cellphone.id}>
+                        <h2>{cellphone.name}</h2>
+                        <p>
+                            {cellphone.brand} - {cellphone.model}
+                        </p>
+                        <p>Opções:</p>
+                        <div className="ml-1">
+                            {cellphone.options.map((option) => 
+                                <div key={option.id}>
+                                    <p>Cor: {option.color}</p>
+                                    <p>Preço: {option.price}</p>
+                                </div>
+                            )}
                         </div>
-                    )}
-                    <Link href={`/cellphones/${cellphone.id}/edit`}>
-                        <button className="rounded-sm border-2 px-2 hover:bg-slate-50">Editar</button>
-                    </Link>
-                </div>
-            )}
+                        <Link href={`/cellphones/${cellphone.id}/edit`}>
+                            <button className="rounded-sm border-2 px-2 hover:bg-slate-50">Editar</button>
+                        </Link>
+                        <DeleteProductButtom id={cellphone.id} />
+                    </div>
+                )}
+            </div>
         </main>
     );
 }
