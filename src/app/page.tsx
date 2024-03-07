7c98d98fe1cd81363228d9da3b9a179c7c1ba783
Henrik Santos <henrik.ruan4@gmail.com>
2024-03-07 09:04:59 -0300

    💄register page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import React, { ChangeEvent, FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
 import loginUser from "@/api/loginUser";
 import Link from "next/link";
+import lexWhite from "../../public/lex-white.svg";
+import Image from "next/image";
 
 export default function Login() {
     const [email, setEmail] = useState<string>("");
@@ -36,25 +38,30 @@ export default function Login() {
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleLogin}>
+        <main className="mx-auto my-72 w-10/12 md:w-4/12">
+            <Image className="my-7" src={lexWhite} alt="Lexart logo" width={200} height={200}/>
+
+            <h2 className="my-2 text-xl">Login</h2>
+            <form className="flex flex-wrap gap-3" onSubmit={handleLogin}>
                 <input
+                    className="w-full p-2"
                     type="email"
                     placeholder="Email"
                     value={email}
                     onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
                 />
                 <input
+                    className="w-full p-2"
                     type="password"
                     placeholder="Senha"
                     value={password}
                     onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
                 />
-                <button type="submit">Entrar</button>
+                <button className="gradient-border w-full border p-2 hover:bg-white/20" type="submit">Entrar</button>
             </form>
-            {message && <p>{message}</p>}
-            <Link href={"/register"}>Criar conta</Link>
-        </div>
+            <br />
+            <Link className="mx-auto w-full self-center border p-2 text-center hover:cursor-pointer hover:bg-white/20" href={"/register"}>Criar conta</Link>
+            {message && <p className="my-3">{message}</p>}
+        </main>
     );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,6 +3,9 @@
 import React, { ChangeEvent, FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
 import registerUser from "@/api/registerUser";
+import lexWhite from "../../../public/lex-white.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Register() {
     const [email, setEmail] = useState<string>("");
@@ -43,33 +46,39 @@ export default function Register() {
     };
 
     return (
-        <div>
-            <h2>Registro</h2>
-            <form onSubmit={handleRegister}>
+        <main className="mx-auto my-72 w-10/12 md:w-4/12">
+            <Image className="my-7" src={lexWhite} alt="Lexart logo" width={200} height={200}/>
+            <h2 className="my-2 text-xl">Registro:</h2>
+            <form className="flex flex-wrap items-start justify-between gap-3" onSubmit={handleRegister}>
                 <input
+                    className="w-full p-2"
                     type="email" 
                     placeholder="Email" 
                     value={email} 
                     onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} 
                     required 
                 />
-                <input 
+                <input
+                    className="w-full p-2" 
                     type="password" 
                     placeholder="Senha" 
                     value={password} 
                     onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} 
                     required 
                 />
-                <input 
+                <input
+                    className="w-full p-2" 
                     type="password" 
                     placeholder="Confirme a senha" 
                     value={confirmPassword} 
                     onChange={(e: ChangeEvent<HTMLInputElement>) => setConfirmPassword(e.target.value)} 
                     required 
                 />
-                <button type="submit">Registrar</button>
+                <button className="gradient-border w-full border p-2 hover:bg-white/20" type="submit">Registrar</button>
             </form>
-            {message && <p>{message}</p>}
-        </div>
+            <br />
+            <Link className="border p-2 hover:cursor-pointer hover:bg-white/20" href={"/"}>Login</Link>
+            {message && <p className="my-3">{message}</p>}
+        </main>
     );
 }
